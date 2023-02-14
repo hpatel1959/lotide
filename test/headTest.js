bedf6190df1cc87aa239aeee5ab8b75c80393e03
Harsh Patel <hpatel1959@hotmail.com>
2023-02-14 03:49:59 +0000

    added mocha and chai, and converted our test cases from test/headTest.js to mocha/chai tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,27 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([]));
-assertEqual(head([1]), 1);
-assertEqual(head([1]), 3);
+describe('---- Tests cases for head.js', () => {
+
+  it ('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it ("should return Hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it ('should return undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it ('should return 1 for [1]', () => {
+    assert.strictEqual(head([1]), 1);
+  });
+
+  it ('should return 1 for [1] causing assertion will fail because 1 !== 3', () => {
+    assert.notEqual(head([1]), 3);
+  });
+
+});
