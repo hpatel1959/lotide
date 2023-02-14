1dd892086d498d1210932fc230126b1b6a9b9e07
Harsh Patel <hpatel1959@hotmail.com>
2023-02-14 04:11:46 +0000

    fixed a type in it statement for middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -9,7 +9,7 @@ const assert = require('chai').assert;
 
 describe('---- Test cases for middle.js----', () => {
 
-  it ('should return [3, 4] for [1, 2, 3, 4, 5]', () => {
+  it ('should return [3, 4] for [1, 2, 3, 4, 5, 6]', () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 
